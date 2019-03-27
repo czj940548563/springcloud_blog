a5ec05fcd458bd7c246230b2fc62b8371db2d4e6
czj940548563 <940548563@qq.com>
2019-03-27 14:02:24 +0800

    提交blogparent

---
+++ blog-parent/src/test/java/com/czj/blog/blogparent/BlogParentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.czj.blog.blogparent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlogParentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
